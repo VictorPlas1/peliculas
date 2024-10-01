a441fa9ce04114df34331eb4b86259c02dc3be03
Victor Plas <victor-real@hotmail.es>
2024-10-01 16:34:24 +0200

    hola

---
+++ lib/homescreen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'buscarWidget.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 215, 7, 7),
+            title: const Text(
+              'PELICULAS',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            centerTitle: true,
+            elevation: 10,
+            shadowColor: Colors.orange,
+            toolbarHeight: 70,
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.more_vert,
+                    size: 40,
+                  )),
+            ],
+            leading: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.home,
+                  size: 40,
+                ))),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Buscarwidget(),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Más populares'),
+                      Text('Ver todas'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 180,
+                        width: 120,
+                        decoration: const BoxDecoration(
+                          //imagen de la caratula
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Container(
+                        height: 180,
+                        width: 120,
+                        decoration: const BoxDecoration(
+                          //imagen de la caratula
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Container(
+                        height: 180,
+                        width: 120,
+                        decoration: const BoxDecoration(
+                          //imagen de la caratula
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ]),
+          ),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:peliculas/buscarWidget.dart';
+import 'homescreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,70 +18,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 232, 177, 177),
-          title: const Text(
-            'PELICULAS',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          centerTitle: true,
-          elevation: 10,
-          shadowColor: Colors.red,
-          toolbarHeight: 70,
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.more_vert,
-                  size: 40,
-                )),
-          ],
-          leading: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.home,
-                size: 40,
-              ))),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Buscarwidget(),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              child: const Text('Hola'),
-              height: 200,
-              decoration: const BoxDecoration(color: Colors.orange),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              child: const Text('Mundo'),
-              height: 150,
-              decoration: const BoxDecoration(color: Colors.blue),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(child: const Text('Mundo'))
-          ],
-        ),
-      ),
     );
   }
 }
